9951f0f829f5c3e9982221f94862ac1d73bc3c61
Pedro Antônio Santos Lima <48067351+pedroantonio150@users.noreply.github.com>
2019-07-12 20:57:15 -0300

    Melhor organização

--- Projeto-Gerencia-Funcionarios/funçoes.h
+++ Projeto-Gerencia-Funcionarios/funçoes.h
@@ -69,34 +69,40 @@ void cadastro()
     }
     menu();
 };
-void consulta(){
+void consulta()
+{
     funcionario funcionariolista;
     ifstream lista("lista.txt", ios::in );
-    if (lista.fail()){
+    if (lista.fail())
+    {
         cerr<< "Lista nao aberto";
     }
     cout << setiosflags(ios::left) <<setw(7) << "ID" << setw(20)
-    << "Nome" << setw(5) << "Sexo" << setw(15) << "Local"
-    << setw(20) << "Cargo" << resetiosflags(ios::left)<<setw(10) << "Salario" << endl;
+         << "Nome" << setw(5) << "Sexo" << setw(15) << "Local"
+         << setw(20) << "Cargo" << resetiosflags(ios::left)<<setw(10) << "Salario" << endl;
 
     lista.read((char *)(&funcionariolista),sizeof(funcionario));
-    while(lista && !lista.eof()){
-        if (funcionariolista.id != 0){
+    while(lista && !lista.eof())
+    {
+        if (funcionariolista.id != 0)
+        {
             cout<<setiosflags(ios::left)
-            <<setw(7)<< funcionariolista.id << setw(20) << funcionariolista.nome << setw(5) << funcionariolista.sexo
-            << setw(15) << funcionariolista.local << setw(20) << funcionariolista.cargo << resetiosflags(ios::left)<<setw(10)<< setprecision(2)
-            << setiosflags(ios::fixed)<<setiosflags(ios::showpoint)<<funcionariolista.salario<<endl;
+                <<setw(7)<< funcionariolista.id << setw(20) << funcionariolista.nome << setw(5) << funcionariolista.sexo
+                << setw(15) << funcionariolista.local << setw(20) << funcionariolista.cargo << resetiosflags(ios::left)<<setw(10)<< setprecision(2)
+                << setiosflags(ios::fixed)<<setiosflags(ios::showpoint)<<funcionariolista.salario<<endl;
         }
-            lista.read((char *)(&funcionariolista),sizeof(funcionario));
+        lista.read((char *)(&funcionariolista),sizeof(funcionario));
     }
     cout << endl;
     lista.close();
-    if (lista.is_open()){
+    if (lista.is_open())
+    {
         cerr << "Lista nao fechado"<<endl;
     }
     menu();
 }
-void menu(){
+void menu()
+{
     cout << "1-Preparar novo arquivo"<<endl;
     cout << "2-Cadastrar funcionario" << endl;
     cout << "3-Consultar funcionarios cadastrados"<< endl;
@@ -106,22 +112,30 @@ void menu(){
     cout << "Escolha uma opcao valida: "<<endl;
     int n;
     cin>> n;
-    switch (n){
-        case 1: criarblocos();
-                    break;
-        case 2: cadastro();
+    switch (n)
+    {
+    case 1:
+        criarblocos();
         break;
-        case 3:consulta();
+    case 2:
+        cadastro();
         break;
-        case 4:alterasalario();
+    case 3:
+        consulta();
         break;
-        case 5:excluir();
+    case 4:
+        alterasalario();
         break;
-        default: cout<< "Encerrando programa..."<<endl;
+    case 5:
+        excluir();
+        break;
+    default:
+        cout<< "Encerrando programa..."<<endl;
         break;
     }
 }
-void alterasalario(){
+void alterasalario()
+{
     funcionario funcionarionovo,funcionarioalterado;
     fstream lista("lista.txt", ios::in | ios::out | ios::ate );
     if(lista.fail())
@@ -136,7 +150,7 @@ void alterasalario(){
         cin >> funcionarionovo.salario;
         lista.seekg((funcionarionovo.id - 1)*sizeof(funcionario));
         float salario= funcionarionovo.salario;
-            lista.read((char *)(&funcionarionovo),sizeof(funcionario));
+        lista.read((char *)(&funcionarionovo),sizeof(funcionario));
         lista.seekp((funcionarionovo.id - 1)*sizeof(funcionario));
         funcionarioalterado=funcionarionovo;
         funcionarionovo= {0,' ',' ',' ',' ',0.0};
@@ -158,7 +172,8 @@ void alterasalario(){
     }
     menu();
 };
-void excluir(){
+void excluir()
+{
     funcionario funcionarionovo= {0,' ',' ',' ',' ',0.0};
     ofstream lista("lista.txt", ios::out | ios::ate );
     if(lista.fail())
@@ -167,12 +182,12 @@ void excluir(){
     }
     cout << "Digite o id do funcionario: " << endl;
     cin >> funcionarionovo.id;
-     while (funcionarionovo.id > 0 && funcionarionovo.id <= 100)
+    while (funcionarionovo.id > 0 && funcionarionovo.id <= 100)
     {
         lista.seekp((funcionarionovo.id - 1)*sizeof(funcionario));
         funcionarionovo.id=0;
         lista.write((const char*)(&funcionarionovo), sizeof(funcionario));
-                cout << "Deseja excluir outro funcionario? (1-sim)" << endl;
+        cout << "Deseja excluir outro funcionario? (1-sim)" << endl;
         int i;
         cin >> i;
         if (i!=1)
